8f411cf2b73d91a8a3c04cf0058e6f3568d21538
Duong Manh Hung <22010106@st.phenikaa-uni.edu.vn>
2024-05-20 11:33:11 +0700

    sua js trang chu

--- trangchu.js
+++ trangchu.js
@@ -19,20 +19,20 @@ fetch(
       const mgg = document.createElement("div");
       mgg.className = "mgg";
 
-      mgg.innerHTML = `
-    <a href="#"></a>
-    <div class="voucher-main" style="display: flex; justify-content: center;">
-      <div class="info-voucher"> 
-        <div><span>Giảm giá: ${voucher.name}</span></div>
-        <div><progress value="${voucher.start}" max="${voucher.end}"></progress></div>
-        <div><span>Chi tiết</span></div>
-      </div>
-      <div class="action-voucher">
-        <button class="btn">Dùng ngay</button>
-        <button class="btn">List áp dụng</button>
-      </div>
-    </div>
-  `;
+  //     mgg.innerHTML = `
+  //   <a href="#"></a>
+  //   <div class="voucher-main" style="display: flex; justify-content: center;">
+  //     <div class="info-voucher"> 
+  //       <div><span>Giảm giá: ${voucher.name}</span></div>
+  //       <div><progress value="${voucher.start}" max="${voucher.end}"></progress></div>
+  //       <div><span>Chi tiết</span></div>
+  //     </div>
+  //     <div class="action-voucher">
+  //       <button class="btn">Dùng ngay</button>
+  //       <button class="btn">List áp dụng</button>
+  //     </div>
+  //   </div>
+  // `;
 
       board.appendChild(mgg);
     });
