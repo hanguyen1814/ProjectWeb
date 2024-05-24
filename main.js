62e9cb396c77e408144faacb5acb3597f8c5deae
Ha Nguyen <hungha31123@gmail.com>
2024-05-24 17:00:27 +0700

    fix css

--- main.js
+++ main.js
@@ -11,8 +11,41 @@ function formatTimestamp(timestamp) {
     hours + ":" + minutes.substr(-2) + " " + day + "/" + month + "/" + year;
   return formattedTime;
 }
+// button prev-next
+document.addEventListener("DOMContentLoaded", () => {
+  const track = document.querySelector(".banner-track");
+  const banners = track.querySelectorAll("img");
+  const totalImages = banners.length / 2;
+  let currentIndex = 0;
 
-console.log(formatTimestamp(1716310740)); // Output: "HH:MM dd/mm/yyyy"
+  function updateTrackPosition() {
+    track.style.transform = `translateX(-${currentIndex * (100 / 4)}%)`;
+  }
+
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % totalImages;
+    updateTrackPosition();
+  }
+
+  function prevImage() {
+    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+    updateTrackPosition();
+  }
+
+  updateTrackPosition();
+
+  setInterval(nextImage, 5000);
+
+  document.querySelector(".next-button").addEventListener("click", nextImage);
+  document.querySelector(".prev-button").addEventListener("click", prevImage);
+
+  banners.forEach((banner) => {
+    const clone = banner.cloneNode(true);
+    track.appendChild(clone);
+  });
+});
+
+// console.log(formatTimestamp(1716310740)); // Output: "HH:MM dd/mm/yyyy"
 
 const requestOptions = {
   method: "GET",
