cd9e985f0a215599edcaa7d470cef73adf94ec7f
Nguyen Hai Nam <hainam13122004@gmail.com>
2024-05-28 14:14:59 +0700

    back to top button

--- main.js
+++ main.js
@@ -65,6 +65,7 @@ fetch(
   })
   .catch((error) => console.error(error));
   document.addEventListener("DOMContentLoaded", () => {
+<<<<<<< Updated upstream
     const track = document.querySelector(".banner-track");
     const banners = track.querySelectorAll("img");
     const totalImages = banners.length / 2;
@@ -118,3 +119,56 @@ fetch(
     document.querySelector(".prev-button").addEventListener("click", prevImage);
   });
   
+=======
+   const track = document.querySelector(".banner-track");
+   const banners = track.querySelectorAll("img");
+   const totalImages = banners.length / 2;
+   let currentIndex = 0;
+ 
+   function updateTrackPosition() {
+     track.style.transform = `translateX(-${currentIndex * (100 / 4)}%)`;
+   }
+ 
+   function nextImage() {
+     currentIndex = (currentIndex + 1) % totalImages;
+     updateTrackPosition();
+   }
+ 
+   function prevImage() {
+     currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+     updateTrackPosition();
+   }
+ 
+   updateTrackPosition();
+ 
+   setInterval(nextImage, 5000);
+ 
+   document.querySelector(".next-button").addEventListener("click", nextImage);
+   document.querySelector(".prev-button").addEventListener("click", prevImage);
+ 
+   banners.forEach((banner) => {
+     const clone = banner.cloneNode(true);
+     track.appendChild(clone);
+   });
+ });
+
+//  back to top
+
+document.addEventListener("DOMContentLoaded", () => {
+  const backToTopBtn = document.getElementById("backToTopBtn");
+
+  window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      backToTopBtn.style.display = "block";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  };
+
+  backToTopBtn.addEventListener("click", () => {
+    document.documentElement.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+
+>>>>>>> Stashed changes
