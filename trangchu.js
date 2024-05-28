4093a4bb200eba39f8ed3c6f44626c72251b2434
Nguyen Hai Nam <hainam13122004@gmail.com>
2024-05-28 14:18:33 +0700

    btb trangchu

--- trangchu.js
+++ trangchu.js
@@ -51,3 +51,49 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".next-button").addEventListener("click", nextImage);
   document.querySelector(".prev-button").addEventListener("click", prevImage);
 });
+
+// back to top
+
+var sttElem = document.querySelector('.stt');
+var screanHeight = window.innerHeight;
+
+var sttScroll = function sttScroll() {
+  document.addEventListener('scroll', function (e) {
+    if (screanHeight <= window.scrollY) {
+      sttElem.classList.add('stt__active');
+    } else if (e.target.scrollingElement.scrollTop <= screanHeight) {
+      sttElem.classList.remove('stt__active');
+      sttElem.style.pointerEvents = 'auto';
+    }
+  });
+};
+
+var sttClick = function sttClick() {
+  sttElem.addEventListener('click', function () {
+    var docHeight = window.scrollY;
+    var progress = 0;
+    var position = docHeight;
+    var speed = 5;
+
+    sttElem.style.pointerEvents = 'none';
+
+    var sttAnim = function sttAnim() {
+      progress += 1;
+      position -= progress * speed;
+      window.scrollTo(0, position);
+
+      if (position > 0) {
+        requestAnimationFrame(sttAnim);
+      }
+    };
+
+    requestAnimationFrame(sttAnim);
+  });
+};
+
+var sttFunc = function sttFunc() {
+  sttScroll();
+  sttClick();
+};
+
+document.addEventListener('DOMContentLoaded', sttFunc);
